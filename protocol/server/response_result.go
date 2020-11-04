81df32ff3541781180ddc1ec96f23c031a27f782
昌维 <867597730@qq.com>
2020-11-04 16:19:24 +0800

    add response_result packet comment

--- protocol/server/response_result.go
+++ protocol/server/response_result.go
@@ -10,6 +10,49 @@ type ResponseResultSet struct {
 	ResponseRowData []ResponseRowData
 }
 
+/**
+Frame 104: 454 bytes on wire (3632 bits), 454 bytes captured (3632 bits) on interface unknown, id 0
+Null/Loopback
+Internet Protocol Version 4, Src: 127.0.0.1, Dst: 127.0.0.1
+Transmission Control Protocol, Src Port: 3306, Dst Port: 4447, Seq: 26566, Ack: 3860, Len: 410
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+    Packet Length: 50
+    Packet Number: 3
+    Catalog: def
+    Database: test
+    Table: user
+    Original table: user
+    Name: username
+    Original name: username
+    Charset number: utf8mb4 COLLATE utf8mb4_general_ci (45)
+    Length: 1020
+    Type: FIELD_TYPE_VAR_STRING (253)
+    Flags: 0x1001
+        .... .... .... ...1 = Not null: Set
+        .... .... .... ..0. = Primary key: Not set
+        .... .... .... .0.. = Unique key: Not set
+        .... .... .... 0... = Multiple key: Not set
+        .... .... ...0 .... = Blob: Not set
+        .... .... ..0. .... = Unsigned: Not set
+        .... .... .0.. .... = Zero fill: Not set
+        .... .... 0... .... = Binary: Not set
+        .... ...0 .... .... = Enum: Not set
+        .... ..0. .... .... = Auto increment: Not set
+        .... .0.. .... .... = Timestamp: Not set
+        .... 0... .... .... = Set: Not set
+    Decimals: 0
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+MySQL Protocol
+
+ */
+
 type ResponseField struct {
 	Catalog       string
 	Database      string
